653118fae288f4208a6eda4bd82ca2184b541a72
stevecooley <steve@sc-fa.com>
2023-09-06 22:44:57 -0700

    Update process_all_videos_into_timelapse_compilation.py
    added some additional feedback and time elapsed metrics

--- process_all_videos_into_timelapse_compilation.py
+++ process_all_videos_into_timelapse_compilation.py
@@ -1,7 +1,9 @@
 import cv2
 import os
+from time import time
 
-def time_lapse_video(input_path, output_path, frame_step):
+def time_lapse_video(input_path, output_path, frame_step, current, total):
+    start_time = time()
     cap = cv2.VideoCapture(input_path)
 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -24,26 +26,33 @@ def time_lapse_video(input_path, output_path, frame_step):
     cap.release()
     out.release()
 
+    # Calculate processing time and file sizes
+    process_time = time() - start_time
+    original_size = os.path.getsize(input_path) / (1024 * 1024)  # Convert bytes to megabytes
+    new_size = os.path.getsize(output_path) / (1024 * 1024)  # Convert bytes to megabytes
+    print(f"Finished processing '{input_path}' ({current} of {total}). Original size: {original_size:.2f} MB, New size: {new_size:.2f} MB, Processing time: {process_time:.2f} seconds")
+
 def combine_videos(video_paths, output_path):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = None
 
     for video_path in video_paths:
-        cap = cv2.VideoCapture(video_path)
+        if os.path.getsize(video_path) > 102400:  # Skip files smaller than 100 KB
+            cap = cv2.VideoCapture(video_path)
 
-        if out is None:
-            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            out = cv2.VideoWriter(output_path, fourcc, 30.0, (width, height))
+            if out is None:
+                width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                out = cv2.VideoWriter(output_path, fourcc, 30.0, (width, height))
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if ret:
-                out.write(frame)
-            else:
-                break
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if ret:
+                    out.write(frame)
+                else:
+                    break
 
-        cap.release()
+            cap.release()
 
     if out is not None:
         out.release()
@@ -52,16 +61,18 @@ if __name__ == "__main__":
     directory = '.'
     output_videos = []
 
-    for filename in sorted(os.listdir(directory), reverse=False):
-        if filename.endswith(".mp4"):
-            input_path = os.path.join(directory, filename)
-            output_path = "time_lapsed_" + filename
-            output_videos.append(output_path)
-            # hey you can change the '150" in the next line to change how many frames it should drop
-            # before it picks up a frame to include. So for instance, 
-            # if you set it to 1, it will drop 1 frame, and pick up one. (faster)
-            # if you set it to 10, it will drop 10 frames, and pick up one. (much faster)
-            # if you set it to 150, it will drop 150 frames and pick up one. (super turbo faster)
-            time_lapse_video(input_path, output_path, 150)
+    mp4_files = [filename for filename in sorted(os.listdir(directory), reverse=False) if filename.endswith(".mp4")]
+    total_files = len(mp4_files)
+
+    for index, filename in enumerate(mp4_files, start=1):
+        input_path = os.path.join(directory, filename)
+        output_path = "time_lapsed_" + filename
+        output_videos.append(output_path)
+        # hey you can change the '150" in the next line to change how many frames it should drop
+        # before it picks up a frame to include. So for instance, 
+        # if you set it to 1, it will drop 1 frame, and pick up one. (faster)
+        # if you set it to 10, it will drop 10 frames, and pick up one. (much faster)
+        # if you set it to 150, it will drop 150 frames and pick up one. (super turbo faster)
+        time_lapse_video(input_path, output_path, 150, index, total_files)
 
     combine_videos(output_videos, "combined_time_lapse.mp4")
